7e8780b85d09758eb806e83a8f5491798cf46e1d
mnttea <mntteass@gmail.com>
2020-08-22 09:26:45 -0700

    Updates

---
+++ precache-manifest.08e89d0ab8d6880dc277f3edb503fc7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6b68c5f30aa125b881518f0c226c37",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "9ecb4a50b9accb96b6ee",
+    "url": "/monstersRolodex/static/css/main.8a84df50.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersRolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersRolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ecb4a50b9accb96b6ee",
+    "url": "/monstersRolodex/static/js/main.abdf7151.chunk.js"
+  },
+  {
+    "revision": "0735bc06010632169b4e",
+    "url": "/monstersRolodex/static/js/runtime-main.0dd8b790.js"
+  }
+]);
